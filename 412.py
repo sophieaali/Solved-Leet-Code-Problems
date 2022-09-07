b4b5b98794de2d903ecaa9ceabb7e9ba1ee0f570
Sophie <sophieaali99@gmail.com>
2022-09-06 22:01:51 -0400

    update to 412

--- 412.py
+++ 412.py
@@ -6,6 +6,7 @@
 # answer[i] == "Buzz" if i is divisible by 5.
 # answer[i] == i (as a string) if none of the above conditions are true.
 
+# solution 1:
 def fizzBuzz(n):
     newList = []
     for i in range(1, n+1):
@@ -19,6 +20,36 @@ def fizzBuzz(n):
             newList.append(str(i))
     return newList
  
+# Example 1:
+print(fizzBuzz(3))
+# Output: ["1","2","Fizz"]
+
+# Example 2:
+print(fizzBuzz(5))
+# Output: ["1","2","Fizz","4","Buzz"]
+
+# Example 3:
+print(fizzBuzz(15))
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+
+
+# solution 2:
+def fizzBuzz(n):
+    ansList = []
+    for i in range(1, n+1):
+        ansItem = ''
+        if i%3 == 0:
+            ansItem += 'Fizz'
+        if i%5 == 0:
+            ansItem += 'Buzz'
+        if not (i%5 == 0) and not (i%3 == 0):
+            ansItem += str(i)
+        ansList.append(ansItem)
+
+    return ansList
+
+
 
 # Example 1:
 print(fizzBuzz(3))
@@ -31,4 +62,5 @@ print(fizzBuzz(5))
 # Example 3:
 print(fizzBuzz(15))
 # Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
  
