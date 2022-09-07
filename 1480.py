1f6e81c707d84a148abbf375edb36e246003c4a7
Sophie <sophieaali99@gmail.com>
2022-09-06 20:21:54 -0400

    upload by Sophie

---
+++ 1480.py
@@ -0,0 +1,30 @@
+# Running Sum of 1d Array
+
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+
+def runningSum(nums):
+    newList = []
+    for x in range(len(nums)):
+        newList.append(sum(nums[0:x+1]))
+    return newList
+
+print(runningSum([1,2,3,4]))
+# Output: [1,3,6,10]
+
+print(runningSum([1,1,1,1,1]))
+# Output: [1,2,3,4,5]
+
+def runningSum(nums):
+    newList = []
+    result = 0
+    for x in nums:
+        result += x
+        newList.append(result)
+    return newList
+
+print(runningSum([1,2,3,4]))
+# Output: [1,3,6,10]
+
+print(runningSum([1,1,1,1,1]))
+# Output: [1,2,3,4,5]
