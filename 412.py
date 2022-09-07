35919364005ca36d68af26c95ff8985e1cb63ef9
Sophie <sophieaali99@gmail.com>
2022-09-06 21:32:19 -0400

    my solution to FizzBuzz problem

---
+++ 412.py
@@ -0,0 +1,34 @@
+# Fizz Buzz
+
+# Given an integer n, return a string array answer (1-indexed) where:
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+def fizzBuzz(n):
+    newList = []
+    for i in range(1, n+1):
+        if (i%3 == 0) and (i%5 == 0):
+            newList.append("FizzBuzz")
+        elif i%3 == 0:
+            newList.append("Fizz")
+        elif i%5 == 0:
+            newList.append("Buzz")
+        else:
+            newList.append(str(i))
+    return newList
+ 
+
+# Example 1:
+print(fizzBuzz(3))
+# Output: ["1","2","Fizz"]
+
+# Example 2:
+print(fizzBuzz(5))
+# Output: ["1","2","Fizz","4","Buzz"]
+
+# Example 3:
+print(fizzBuzz(15))
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
